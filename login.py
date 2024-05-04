98f5452cbd04d05c0da37116d5dd25ff7e669c91
conquru <zconquru@gmail.com>
2024-05-04 11:43:45 +0300

    add history

--- login.py
+++ login.py
@@ -28,13 +28,13 @@ def load_user(user_id):
 @login_required
 def logout():
     logout_user()
-    return redirect('/login')
+    return redirect('/')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect('/search')
+        return redirect('/result')
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
@@ -53,63 +53,71 @@ def search():
     form = SearchForm()
     
     if form.validate_on_submit():
-        tickets = []
-        result = GetTickets(form.city_start.data, form.city_end.data, form.time.data)
-        for i in result:
-            tickets.append(' '.join(i))
-        print(tickets)
-        if tickets == []:
-            time.sleep(2)
-            tickets = []
-            result = GetTickets(form.city_start.data, form.city_end.data, form.time.data)
-            for i in result:
-                tickets.append(' '.join(i))
-            if tickets == []:
-                list_tickets = 'Мы не смогли найти нужные билеты, можете обратится в службу поддержкии(которой нет)'
-            else:
-                list_tickets = ' '.join(tickets)
-        else:
-            list_tickets = ' '.join(tickets)
+        # tickets = []
+        # result = GetTickets(form.city_start.data, form.city_end.data, form.time.data)
+        # for i in result:
+        #     tickets.append(' '.join(i))
+        # print(tickets)
+        # if tickets == []:
+        #     time.sleep(2)
+        #     tickets = []
+        #     result = GetTickets(form.city_start.data, form.city_end.data, form.time.data)
+        #     for i in result:
+        #         tickets.append(' '.join(i))
+        #     if tickets == []:
+        #         list_tickets = 'Мы не смогли найти нужные билеты, можете обратится в службу поддержкии(которой нет)'
+        #     else:
+        #         list_tickets = ' '.join(tickets)
+        # else:
+        #     list_tickets = ' '.join(tickets)
         
-        entrers = []
-        result = GetEntertainments(form.city_end.data)
-        for i in result:
-            entrers.append(' '.join(i))
-        print(entrers)
-        if entrers == []:
-            time.sleep(2)
-            entrers = []
-            result = GetEntertainments(form.city_end.data)
-            for i in result:
-                entrers.append(' '.join(i))
-            if entrers == []:
-                list_entrers = 'Мы не смогли найти нужную информацию, можете обратится в службу поддержкии(которой нет)'
-            else:
-                list_entrers = ' '.join(entrers)
-        else:
-            list_entrers = ' '.join(entrers)
+        # entrers = []
+        # result = GetEntertainments(form.city_end.data)
+        # for i in result:
+        #     entrers.append(' '.join(i))
+        # print(entrers)
+        # if entrers == []:
+        #     time.sleep(2)
+        #     entrers = []
+        #     result = GetEntertainments(form.city_end.data)
+        #     for i in result:
+        #         entrers.append(' '.join(i))
+        #     if entrers == []:
+        #         list_entrers = 'Мы не смогли найти нужную информацию, можете обратится в службу поддержкии(которой нет)'
+        #     else:
+        #         list_entrers = ' '.join(entrers)
+        # else:
+        #     list_entrers = ' '.join(entrers)
 
-        hotel = []
-        result = GetHotel(form.city_end.data)
-        for i in result:
-            hotel.append(' '.join(i))
-        print(hotel)
-        if hotel == []:
-            time.sleep(2)
-            hotel = []
-            result = GetHotel(form.city_end.data)
-            for i in result:
-                hotel.append(' '.join(i))
-                hotel = GetHotel(form.city_end.data)
-            if hotel == []:
-                list_hotel = 'Мы не смогли найти нужную информацию, можете обратится в службу поддержкии(которой нет)'
-            else:
-                list_hotel = ' '.join(hotel)
-        else:
-            list_hotel = ' '.join(hotel)
-
+        # hotel = []
+        # result = GetHotel(form.city_end.data)
+        # for i in result:
+        #     hotel.append(' '.join(i))
+        # print(hotel)
+        # if hotel == []:
+        #     time.sleep(2)
+        #     hotel = []
+        #     result = GetHotel(form.city_end.data)
+        #     for i in result:
+        #         hotel.append(' '.join(i))
+        #         hotel = GetHotel(form.city_end.data)
+        #     if hotel == []:
+        #         list_hotel = 'Мы не смогли найти нужную информацию, можете обратится в службу поддержкии(которой нет)'
+        #     else:
+        #         list_hotel = ' '.join(hotel)
+        # else:
+        #     list_hotel = ' '.join(hotel)
+        tickets = ['test', 'test', 'test']
+        list_tickets = ' '.join(tickets)
+        hotel = ['test', 'test', 'test']
+        list_hotel = ' '.join(hotel)
+        entrers = ['test', 'test', 'test']
+        list_entrers = ' '.join(entrers)
         db_sess = db_session.create_session()
+        user = User()
+        now_user = db_sess.query(User).filter(User.name == current_user.name).first()
         history = History()
+        history.name = now_user.id
         history.tickets = list_tickets
         history.enter = list_entrers
         history.hotel = list_hotel
@@ -119,36 +127,27 @@ def search():
         # current_user.history.append(history)
         db_sess.add(history)
         db_sess.commit()
-        return render_template('result.html', tickets=tickets, hotel=hotel, enter=entrers, form=form)
+        return render_template('search.html', tickets=tickets, hotel=hotel, enter=entrers, form=form)
     return render_template('search.html', form=form)
 
 @app.route('/history', methods=['GET', 'POST'])
-def result():
+def history():
     db_sess = db_session.create_session()
-    query_history = db_sess.query(History)
+    user = User()
+    history = History()
     query = db_sess.query(User)
+    list_history = []
     for user in db_sess.query(User).all():
         if user.name == current_user.name:
             user_id = user.id
-    history = db_sess.query_history(History).filter(History.name == user_id)
-
-    return render_template('jobs.html', history=history)
-
-
-@app.route('/authorized', methods=['GET', 'POST'])
-# def main():
-#     global name
-#     db_sess = db_session.create_session()
-#     query_history = db_sess.query(History)
-#     query = db_sess.query(User)
-#     for user in db_sess.query(User).all():
-#         if user.email == name:
-#             user_id = user.id
-#     history = db_sess.query_history(History).filter(History.name == user_id)
-#     return render_template('jobs.html', history=history)
+            print(user_id)
+    query = db_sess.query(History)
+    for i in db_sess.query(History).filter(History.name == user_id):
+        list_history.append(i)
+    return render_template('history.html', history=list_history)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/registr', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -168,10 +167,10 @@ def reqister():
         user.set_password(form.hashed_password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
+        return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    app.run(port=8000, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
